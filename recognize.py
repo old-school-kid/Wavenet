e439defa1c94fe53bb0c9e0e855c306ab25d4671
Surya Prakash Mishra <mishrasp393@gmail.com>
2021-07-08 20:53:40 +0530

    bug in recognize

--- recognize.py
+++ recognize.py
@@ -17,6 +17,8 @@ tf.sg_verbosity(10)
 #
 
 batch_size = 1     # batch size
+num_blocks = 3     # dilated blocks
+num_dim = 128      # latent dimension
 
 #
 # inputs
@@ -29,10 +31,39 @@ voca_size = data.voca_size
 x = tf.placeholder(dtype=tf.sg_floatx, shape=(batch_size, None, 20))
 
 # sequence length except zero-padding
-seq_len = tf.not_equal(x.sg_sum(axis=2), 0.).sg_int().sg_sum(axis=1)
+seq_len = tf.not_equal(x.sg_sum(dims=2), 0.).sg_int().sg_sum(dims=1)
 
-# encode audio feature
-logit = get_logit(x, voca_size=voca_size)
+def res_block(tensor, size, rate, dim=num_dim):
+
+    # filter convolution
+    conv_filter = tensor.sg_aconv1d(size=size, rate=rate, act='tanh', bn=True)
+
+    # gate convolution
+    conv_gate = tensor.sg_aconv1d(size=size, rate=rate,  act='sigmoid', bn=True)
+
+    # output by gate multiplying
+    out = conv_filter * conv_gate
+
+    # final output
+    out = out.sg_conv1d(size=1, dim=dim, act='tanh', bn=True)
+
+    # residual and skip output
+    return out + tensor, out
+
+# expand dimension
+z = x.sg_conv1d(size=1, dim=num_dim, act='tanh', bn=True)
+
+# dilated conv block loop
+skip = 0  # skip connections
+for i in range(num_blocks):
+    for r in [1, 2, 4, 8, 16]:
+        z, s = res_block(z, size=7, rate=r)
+        skip += s
+
+# final logit layers
+logit = (skip
+         .sg_conv1d(size=1, act='tanh', bn=True)
+         .sg_conv1d(size=1, dim=voca_size))
 
 # ctc decoding
 decoded, _ = tf.nn.ctc_beam_search_decoder(logit.sg_transpose(perm=[1, 0, 2]), seq_len, merge_repeated=False)
